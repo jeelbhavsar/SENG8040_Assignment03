10942f92439c190726524444f80ff54b478cba86
jeelbhavsar <sonu23121998@gmail.com>
2021-07-03 01:41:18 +0530

    Initial commit

---
+++ Assignment02_8040/Assignment02_8040/TriangleSolve.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment02_8040
+{
+  public class TriangleSolve
+    {
+        public string Analyze(int A, int B, int C)
+        {
+            if (((A + B) < C) || ((B + C) < C) || ((C + A) < B))
+            {
+                Console.WriteLine("");
+                Console.Write("The triangle can not be formed.\n");
+                return "Can not form Triangle.";
+            }
+
+            else if (((A - B) > C) || ((B - C) > C) || ((C - A) > B))
+            {
+                Console.WriteLine("");
+                Console.Write("The triangle can not be formed.\n");
+                return "Can not form Triangle.";
+            }
+
+            else if ((A == B && B == C) && (A != 0 || B != 0 || C != 0))
+            {
+                Console.WriteLine("");
+                Console.Write("This is an equilateral triangle.\n");
+                return "This is an equilateral triangle.";
+            }
+            else if ((A == B || A == C || B == C) && (A != 0 || B != 0 || C != 0))
+            {
+                Console.WriteLine("");
+                Console.Write("This is an isosceles triangle.\n");
+                return "This is an isosceles triangle.";
+            }
+
+            else if (A == 0 | B == 0 || C == 0)
+            {
+                Console.WriteLine("");
+                Console.Write("The triangle can not be formed.\n");
+                return "Can not form Triangle.";
+            }
+
+            else
+            {
+                Console.WriteLine("");
+                Console.Write("This is a scalene triangle.\n");
+                return "This is a scalene triangle.";
+            }
+        }
+
+    }
+}
---
+++ Assignment02_8040/TriangleTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using System;
+using Assignment02_8040;
+
+namespace TriangleTestProject
+{
+    public class Tests
+    {
+        TriangleSolve myObj = new TriangleSolve();
+        [Test]
+
+        public void NegativeOneSide()
+        {
+            string a;
+            a = myObj.Analyze(-5, 5, 5);
+            Assert.AreEqual(a, "Can not form Triangle.");
+        }
+    }
+}
